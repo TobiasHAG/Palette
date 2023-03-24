56474ffcfbc10de3354f0d42628ff8cf1eb0ad50
TobiasHAG <tobiashag.dev@gmail.com>
2023-03-24 17:09:22 +0100

    Font fixing on main card animation

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
+/* It's a mousemove event listener that moves the gallery element based on the mouse position. */
+
 const gallery = document.getElementById("gallery");
 
 window.onmousemove = e => {
+  /* It's getting the mouse position. */
   const mouseX = e.clientX,
         mouseY = e.clientY;
   
+  /* It's getting the mouse position. */
   const xDecimal = mouseX / window.innerWidth,
         yDecimal = mouseY / window.innerHeight;
   
+  /* It's getting the maximum amount of pixels that the gallery can be moved. */
   const maxX = gallery.offsetWidth - window.innerWidth,
         maxY = gallery.offsetHeight - window.innerHeight;
   
+  /* It's calculating the amount of pixels that the gallery should be moved based on the mouse
+  position. */
   const panX = maxX * xDecimal * -1,
         panY = maxY * yDecimal * -1;
   
+/* It's animating the gallery element. */
   gallery.animate({
     transform: `translate(${panX}px, ${panY}px)`
   }, {
